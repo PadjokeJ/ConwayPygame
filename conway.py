3de72c947c6dd41676f956225ba757dd3e8cf9c9
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-03-19 09:19:08 +0100

    Create conway.py

---
+++ conway.py
@@ -0,0 +1,25 @@
+import pygame
+cell = {"x": 0, "y" : 0}
+oncell = []
+x = []
+y = []
+neighbors =
+(
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+    (0, 1),
+    (0, -1),
+    (1, 1),
+    (1, 0),
+    (1, -1)
+    )
+#declare neighbors:
+x.clear
+y.clear
+for c in oncell :
+    x.append["x"]
+    y.append["y"]
+#check for neighbors:
+for c in oncell :
+    
