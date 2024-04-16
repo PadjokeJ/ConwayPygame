01948d5121d51f5eec5fe5ee046c352ef3da5714
PadjokeJ <jonatanr.pfister@gmail.com>
2024-04-16 09:00:28 +0200

    Cell logic

--- conway.py
+++ conway.py
@@ -1,19 +1,36 @@
 import pygame
 from copy import copy
 gridSize = (10, 10)
-cells = []
-cellsNext = {}
+cells = {}
 def initGrid():
     newCells = {}
     for i in range(gridSize[1]):
         newCells[i] = [copy(False) for i in range(gridSize[1])]
-    cells.append(newCells)
+    return newCells
 
-def checkNeighbors():
-    for i in range(gridSize[1] - 1):
-        for j in range(gridSize[0] - 1):
-            print(i)
-            print(cells[i])
+def checkNeighbors(c):
+    deltaCells = c
+    for i in range(gridSize[1] - 2):
+        for j in range(gridSize[0] - 2):
+            c_x = i + 1
+            c_y = j + 1
+            aliveNeighbors = 0
+            for n in range(8):
+                x = neighbors[n][0]
+                y = neighbors[n][1]
+                cell = cells[c_x + x][c_y + y]
+                if cell:
+                    aliveNeighbors += 1
+            #check if cell is dead
+            if cells[c_x][c_y] == False:
+                if aliveNeighbors == 3:
+                    deltaCells[c_x][c_y] = True
+            else:
+                if aliveNeighbors <= 1:
+                    deltaCells[c_x][c_y] = False
+                if aliveNeighbors >= 4:
+                    deltaCells[c_x][c_y] = False
+    return deltaCells
 
 neighbors = (
     (-1, 1),
@@ -25,7 +42,8 @@ neighbors = (
     (1, 0),
     (1, -1)
     )
-initGrid()
-checkNeighbors()
-cellsNext.clear()
+cells = initGrid()
 
+while True:
+    cells = checkNeighbors(cells)
+    print(cells)
