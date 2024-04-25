89ab03c853749b80c1312496b320842ef7f38157
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-04-25 11:33:12 +0200

    Update screenUpdater.py
    fixed bugs

--- screenUpdater.py
+++ screenUpdater.py
@@ -1,27 +1,25 @@
-
+from copy import copy
+from copy import deepcopy
 def updateScreen(c, _gridSize, _neighbors):
-    deltaCells = c
+    deltaCells = deepcopy(c)
+    originalCells = deepcopy(c)
     for i in range(1, _gridSize[1] - 1):
-        if(i != 0 or i != _gridSize[1]):
-            for j in range(1, _gridSize[0] - 1):
-                if(j != 0 or j != _gridSize[0]):
-                    c_x = i
-                    c_y = j
-                    aliveNeighbors = 0
-                    for n in range(8):
-                        x = _neighbors[n][0]
-                        y = _neighbors[n][1]
-                        # print(c_x, x)
-                        cell = c[c_x + x][c_y + y]
-                        if cell:
-                            aliveNeighbors += 1
-                    #check if cell is dead
-                    if c[c_x][c_y] == False:
-                        if aliveNeighbors == 3:
-                            deltaCells[c_x][c_y] = True
-                        else:
-                            if aliveNeighbors <= 1:
-                                deltaCells[c_x][c_y] = False
-                            if aliveNeighbors >= 4:
-                                deltaCells[c_x][c_y] = False
+        for j in range(1, _gridSize[0] - 1):
+            c_x = i
+            c_y = j
+            aliveNeighbors = 0
+            for n in range(8):
+                x = _neighbors[n][0]
+                y = _neighbors[n][1]
+                # print(c_x, x)
+                cell = originalCells[c_x + x][c_y + y]
+                if cell:
+                    aliveNeighbors += 1
+            #check if cell is dead
+            if aliveNeighbors == 3:
+                deltaCells[c_x][c_y] = True
+            if aliveNeighbors <= 1:
+                deltaCells[c_x][c_y] = False
+            if aliveNeighbors >= 4:
+                deltaCells[c_x][c_y] = False
     return deltaCells
