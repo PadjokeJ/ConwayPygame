f2b534a058460d8ec22be5ff79bf1bef5c376d96
HackFight <88657651+HackFight@users.noreply.github.com>
2024-05-09 10:35:58 +0100

    Better code readability saver.py

--- saver.py
+++ saver.py
@@ -1,21 +1,30 @@
 import json
 
+############################################################################ - Save
 def save (list):
+    
     objects = [0]
+    
     with open("patterns.json") as f:
         objects = json.load(f)
+        
     objects.append(objects[-1] + 1)
+    
     with open("patterns.json", "w") as f:
         json.dump(objects, f)
     
     with open("pattern" + str(objects[-1]) + ".json", "w") as f:
         json.dump(list, f)
 
+############################################################################ - Load
 def load(i):
     with open("patterns.json") as f:
         objects = json.load(f)
+        
     while(objects[-1] -1 < i):
         i = i - len(objects) + 1
+        
     with open("pattern" + str(i) + ".json") as f:
         list = json.load(f)
+        
     return list
