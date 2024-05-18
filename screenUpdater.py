e572fdb3735c6ab70317f362b88c95f813a9a7fe
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-05-18 12:42:19 +0200

    warping

--- screenUpdater.py
+++ screenUpdater.py
@@ -1,24 +1,23 @@
 from copy import copy
 from copy import deepcopy
+def warp(n, min, max):
+    if n < min: n = max
+    if n > max: n = min
+    return n
 def updateScreen(c, _gridSize, _neighbors):
     deltaCells = deepcopy(c)
     originalCells = deepcopy(c)
-    for i in range(1, _gridSize[1] - 1):
-        for j in range(1, _gridSize[0] - 1):
+    for i in range(0, _gridSize[1]):
+        for j in range(0, _gridSize[0]):
             c_x = i
             c_y = j
             aliveNeighbors = 0
             for n in range(8):
-                x = _neighbors[n][0]
-                y = _neighbors[n][1]
-                # print(c_x, x)
-                cell = originalCells[str(c_x + x)][c_y + y]
-                if cell:
-                    aliveNeighbors += 1
-            if aliveNeighbors == 3:
-                deltaCells[str(c_x)][c_y] = True
-            if aliveNeighbors <= 1:
-                deltaCells[str(c_x)][c_y] = False
-            if aliveNeighbors >= 4:
-                deltaCells[str(c_x)][c_y] = False
+                x = warp(_neighbors[n][0] + c_x, 0, _gridSize[0] - 1)
+                y = warp(_neighbors[n][1] + c_y, 0, _gridSize[1] - 1)
+                cell = originalCells[str(x)][y]
+                if cell: aliveNeighbors += 1
+            if aliveNeighbors == 3: deltaCells[str(c_x)][c_y] = True
+            if aliveNeighbors <= 1: deltaCells[str(c_x)][c_y] = False
+            if aliveNeighbors >= 4: deltaCells[str(c_x)][c_y] = False
     return deltaCells
